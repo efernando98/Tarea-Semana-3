0ca9800d0a62bb86a6e74015c71bba571125c6a9
Fernando Olivares <79297097+efernando98@users.noreply.github.com>
2021-02-26 22:46:14 -0600

    Add files via upload

---
+++ Ejercicio1-110/Ejercicio1-110/Program.cs
@@ -0,0 +1,29 @@
+﻿//Hacer un programa que le pida al usuario un número y la computadora responda si es par o impar.
+
+using System;
+
+
+namespace Ejercicio1110
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+				
+	            int numero = 0;
+	            Console.Write("Ingrese un numero para determinar si es par o impar: ");
+	            numero = Convert.ToInt32(Console.ReadLine());
+	
+
+	            if (numero % 2 == 0)
+	            {
+	                Console.WriteLine($"El numero {numero} es par ");
+	            }
+	            else
+	            {
+	                Console.WriteLine($"El numero {numero} es impar");
+	            }
+	        }
+	    }
+	}
